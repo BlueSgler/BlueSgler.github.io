e2bd08109fe007e3a7655e2d744bb6fe0c8ba134
BlueSgler <3032964348@qq.com>
2021-02-04 22:20:41 +0800

    Site updated: 2021-02-04 22:20:40

---
+++ js/Bluesgler.js
@@ -0,0 +1,8 @@
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('2021 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lriver');
+})
+
+// 移除页脚样式
+var app = document.getElementById('footer');
+app.removeAttribute('id')
